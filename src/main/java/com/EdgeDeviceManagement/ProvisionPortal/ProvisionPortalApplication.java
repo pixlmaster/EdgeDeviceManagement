d97588934bfd2c415024cabd1ea27c5b6619655c
unknown <akash.tiwari21aug@gmail.com>
2022-08-31 23:54:16 +0530

    initial commit

---
+++ src/main/java/com/EdgeDeviceManagement/ProvisionPortal/ProvisionPortalApplication.java
@@ -0,0 +1,13 @@
+package com.EdgeDeviceManagement.ProvisionPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisionPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvisionPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EdgeDeviceManagement/ProvisionPortal/ProvisionPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EdgeDeviceManagement.ProvisionPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvisionPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
